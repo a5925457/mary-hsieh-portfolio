f03454379e2d9aff207c89979ff0adc7b3fd6143
Mary Hsieh <a5925457@gmail.com>
2022-02-23 13:30:21 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Fullpage from './components/Fullpage';
-import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Fullpage />} />
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path="/" element={<Fullpage />} />
+        </Routes>
     );
 }
 
